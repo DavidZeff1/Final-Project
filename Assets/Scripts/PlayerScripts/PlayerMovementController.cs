f129619f3e13ad25d076d848da4541aded71df10
RonenGiladov <156519002+RonenGiladov@users.noreply.github.com>
2024-09-21 22:37:19 +0300

    Update Idle animation, Movement
    fixed the animation
    changed the movement
    (change it back if you want)

--- Assets/Scripts/PlayerScripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovementController.cs
@@ -5,30 +5,38 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] Character m_CharacterData;
-    [SerializeField] float m_MovementSpeed;
+    [SerializeField] float m_MovementSpeed = 300f;
     [SerializeField] private Rigidbody2D m_PlayerRigidBody;
     [SerializeField] Animator m_Animator;
     private Vector2 m_Movement;
+    private float m_DeltaX;
+    private float m_DeltaY;
+
+
     private void Start()
     {
-        m_MovementSpeed = m_CharacterData.movementSpeed;
-    }
-
-    private void Update()
-    {
-        m_Movement.x = Input.GetAxisRaw(nameof(InputStr.Horizontal));
-        m_Movement.y = Input.GetAxisRaw(nameof(InputStr.Vertical));
-        //m_Animator.SetFloat(nameof(AnimationParams.MoveX), m_Movement.x);
-        //m_Animator.SetFloat(nameof(AnimationParams.MoveY), m_Movement.y);
-        
+        //m_MovementSpeed = m_CharacterData.movementSpeed;
     }
 
     private void FixedUpdate()
     {
-        m_Animator.SetFloat(nameof(AnimationParams.MoveX), m_Movement.x);
-        m_Animator.SetFloat(nameof(AnimationParams.MoveY), m_Movement.y);
+        m_DeltaX = Input.GetAxis(nameof(InputStr.Horizontal));
+        m_DeltaY = Input.GetAxis(nameof(InputStr.Vertical));
+        
+        m_Movement = new Vector2(m_DeltaX, m_DeltaY) * m_MovementSpeed * Time.deltaTime;
 
-        m_PlayerRigidBody.velocity = (m_Movement) * m_MovementSpeed;       
+        //m_PlayerRigidBody.velocity = (m_Movement) * m_MovementSpeed;       
+        m_PlayerRigidBody.velocity = m_Movement;
+
+        if (m_Movement.magnitude > 0)
+        {
+            m_Animator.SetBool(nameof(AnimationParams.IsMoving), true);
+        }
+        else
+        {
+            m_Animator.SetBool(nameof(AnimationParams.IsMoving), false);
+        }
+
     }
 
     public enum InputStr
@@ -36,9 +44,9 @@ public class PlayerMovementController : MonoBehaviour
         Horizontal,
         Vertical
     }
+    
     enum AnimationParams
     {
-        MoveX,
-        MoveY
+        IsMoving
     }
 }
