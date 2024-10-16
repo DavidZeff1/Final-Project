46bf12e5b11b3f002ed73ccf11461f43a7d27083
RonenGiladov <156519002+RonenGiladov@users.noreply.github.com>
2024-10-16 06:33:14 +0300

    Map Level 2 Lazer PowerUp Update
    Added Level Design for Level 2,
    Power Up Lazer Beam by hitting 'E'

--- Assets/Scripts/SuperPowerShooterController.cs
+++ Assets/Scripts/SuperPowerShooterController.cs
@@ -12,8 +12,12 @@ public class SuperPowerShooterController : MonoBehaviour
     [SerializeField] private float m_ShockwaveForce = 5f;
     [SerializeField] private float m_ShockwaveEffectDuration = 1f;
     [SerializeField] private LayerMask m_EnemyLayer;
+    [SerializeField] private GameObject m_LazerPrefab;
+    [SerializeField] private Transform m_LaserSpawnPoint;
+    [SerializeField] private float m_LazerOffset = 0.5f;
     private const float PowerCooldownDuration = 5f;
     private bool[] m_PowerCooldowns = new bool[4];
+    private GameObject currentLaser;
 
     private void Update()
     {
@@ -36,25 +40,12 @@ public class SuperPowerShooterController : MonoBehaviour
                 break;
             
             case 1:
-                Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, m_ShockwaveRadius, m_EnemyLayer);
+                UseShockWave();
+                break;
 
-                if (m_ShockWavePrefab != null)
-                {
-                    GameObject shockwave = Instantiate(m_ShockWavePrefab, transform.position, Quaternion.identity);
-                    Destroy(shockwave, m_ShockwaveEffectDuration);
-                }
-
-                foreach (Collider2D enemy in enemies)
-                {
-                    Rigidbody2D enemyRb = enemy.GetComponent<Rigidbody2D>();
-                    if (enemyRb != null)
-                    {
-                        Vector2 direction = (enemy.transform.position - transform.position).normalized;
-                        enemyRb.AddForce(direction * m_ShockwaveForce, ForceMode2D.Impulse);
-                        StartCoroutine(LimitVelocity(enemyRb));
-                    }
-                }
-
+            case 2:
+                UseLazer();
+                
                 break;
         }
 
@@ -62,7 +53,40 @@ public class SuperPowerShooterController : MonoBehaviour
         StartCoroutine(PowerCooldownRoutine(powerIndex));
     }
 
-    
+    private void UseShockWave()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, m_ShockwaveRadius, m_EnemyLayer);
+
+        if (m_ShockWavePrefab != null)
+        {
+            GameObject shockwave = Instantiate(m_ShockWavePrefab, transform.position, Quaternion.identity);
+            Destroy(shockwave, m_ShockwaveEffectDuration);
+        }
+
+        foreach (Collider2D enemy in enemies)
+        {
+            Rigidbody2D enemyRb = enemy.GetComponent<Rigidbody2D>();
+            if (enemyRb != null)
+            {
+                Vector2 direction = (enemy.transform.position - transform.position).normalized;
+                enemyRb.AddForce(direction * m_ShockwaveForce, ForceMode2D.Impulse);
+                StartCoroutine(LimitVelocity(enemyRb));
+            }
+        }
+    }
+
+    private void UseLazer()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mousePos - m_LaserSpawnPoint.position).normalized;
+        Vector3 spawnPosition = m_LaserSpawnPoint.position + direction * m_LazerOffset;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        
+        mousePos.z = 0;
+        currentLaser = Instantiate(m_LazerPrefab, spawnPosition, Quaternion.identity);
+        currentLaser.transform.rotation = Quaternion.Euler(0, 0, angle);
+        currentLaser.transform.parent = transform;
+    }
 
     private IEnumerator PowerCooldownRoutine(int powerIndex)
     {
